bea9b34f9dab9661a6623bf321e3646bd518df2d
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-05-17 13:45:42 +0100

    Interactive Menu Commit

--- directory.rb
+++ directory.rb
@@ -40,8 +40,28 @@ def input_students
 
   students
 end
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+    selection = gets.chomp
+
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
